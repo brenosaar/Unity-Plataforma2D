3e92ab5980a417bc56c0d254983a964b8d9084af
brenosaar <81493190+brenosaar@users.noreply.github.com>
2021-03-28 15:17:49 -0300

    updt game levels

--- New Unity Project (2)/Assets/Pixel Adventure 1/Scripts/GameController.cs
+++ New Unity Project (2)/Assets/Pixel Adventure 1/Scripts/GameController.cs
@@ -2,11 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
     public int totalScore;
     public Text scoreText;
+
+    public GameObject gameOver;
 
     public static GameController instance;
 
@@ -20,5 +23,13 @@ public class GameController : MonoBehaviour
         scoreText.text = totalScore.ToString();
     }
 
-  
+    public void ShowGameOver() 
+    {
+        gameOver.SetActive(true);
+    }
+
+    public void RestartGame(string lvlName) 
+    {
+        SceneManager.LoadScene(lvlName);
+    }
 }
--- New Unity Project (2)/Assets/Pixel Adventure 1/Scripts/Player.cs
+++ New Unity Project (2)/Assets/Pixel Adventure 1/Scripts/Player.cs
@@ -76,6 +76,20 @@ public class Player : MonoBehaviour
             isJumping = false;
             anim.SetBool("Jump", false);
         }
+
+        if (collision.gameObject.tag == "Spike")
+        {
+
+
+            GameController.instance.ShowGameOver();
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.tag == "Saw")
+        {
+            GameController.instance.ShowGameOver();
+            Destroy(gameObject);
+        }
     }
 
     void OnCollisionExit2D(Collision2D collision)
